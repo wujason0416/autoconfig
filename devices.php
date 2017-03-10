ac13dc076d336db570003c07eb2806433244a0aa
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-03-10 15:43:28 +0000

    move devices to devices folder ready for seperating out

--- devices.php
+++ devices.php
@@ -2,6 +2,6 @@
 
 function get_devices() {
     global $path;
-    return json_decode(file_get_contents("Modules/autoconfig/devices.json"));
+    return json_decode(file_get_contents("Modules/autoconfig/devices/devices.json"));
 }
 // energy.emoncms.org set configuration, nodename:emontx, configuration:solar
